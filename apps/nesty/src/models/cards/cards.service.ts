62b2b63c13948ad8e2d4da5de339a47a4f9497d9
George Maddocks <georgemaddocks@Georges-MacBook-Pro.local>
2022-03-11 10:40:53 +0000

    Restore cards service file

--- apps/nesty/src/models/cards/cards.service.ts
+++ apps/nesty/src/models/cards/cards.service.ts
@@ -1,21 +1,45 @@
-import { Injectable } from "@nestjs/common";
-
-interface Cat {
-  meow: boolean;
-}
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { QueryRunner, Repository } from 'typeorm';
+import { plainToInstance } from 'class-transformer';
+import { CreateCardDto } from './dto/create-card.dto';
+import { Card } from './entities/card.entity';
+import { BarcodesService } from '../barcodes/barcodes.service';
+import { QueryRunnerService } from '../../database/query-runner.service';
 
 @Injectable()
-export class CatsService {
-  constructor(private queryRunnerService: QueryRunnerService) {}
+export class CardsService {
+  constructor(
+    @InjectRepository(Card)
+    private cardsRepository: Repository<Card>,
+    private queryRunnerService: QueryRunnerService,
+    private barcodesService: BarcodesService,
+  ) {}
 
-  private readonly cats: Cat[] = [];
-
-  create(cat: Cat) {
-    this.cats.push(cat);
+  create(createCardDto: CreateCardDto, userId: string): Promise<Card> {
+    return this.queryRunnerService.withTransaction<Card>(
+      async (queryRunner: QueryRunner) => {
+        const barcode = await this.barcodesService.create();
+        const card = plainToInstance(Card, {
+          message: createCardDto.message,
+          barcodeId: barcode.id,
+          isOpened: false,
+          sender: userId,
+        });
+        console.log('card', card)
+        return queryRunner.manager.save(card);
+      },
+    );
   }
 
-  findAll(): Cat[] {
-    return this.cats;
+  async open(cardId: string): Promise<void> {
+    const card = await this.cardsRepository.findOne({ id: cardId });
+    if (!card)
+      throw new NotFoundException(
+        { cardId },
+        'Card to open not found with supplied cardId',
+      );
+
+    await this.cardsRepository.update({ id: cardId }, { isOpened: true });
   }
 }
-
